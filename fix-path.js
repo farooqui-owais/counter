f8783fdb256f07f71a8e844f58bfab08e8e5a17a
farooqui-owais <farooquiowais70@gmail.com>
2025-02-10 16:44:45 +0530

    fix path

--- fix-path.js
+++ fix-path.js
@@ -1,10 +1,10 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
+const fs = require('fs');
+const path = require('path');
 
-const indexPath = join(__dirname, 'build', 'index.html');
-let content = readFileSync(indexPath, 'utf8');
+const indexPath = path.join(__dirname, 'build', 'index.html');
+let content = fs.readFileSync(indexPath, 'utf8');
 
 // Replace paths with the correct GitHub Pages path
 content = content.replace(/\/assets\//g, './assets/');
 
-writeFileSync(indexPath, content, 'utf8');
+fs.writeFileSync(indexPath, content, 'utf8');
